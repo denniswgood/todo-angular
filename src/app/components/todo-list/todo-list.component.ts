1cae11746d10a29b6fe3e80c5f9bedd3e23c5d21
Dennis Good <dgood@fusionalliance.com>
2019-04-12 23:57:40 -0400

    first file commit

---
+++ src/app/components/todo-list/todo-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements OnInit {
+  todos: object[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.todos = [
+      {
+        'id': 1,
+        'title': 'Finish Angular Screencast',
+        'completed': false,
+        'editing': false,
+      },
+      {
+        'id': 2,
+        'title': 'Take over world',
+        'completed': false,
+        'editing': false,
+      },
+      {
+        'id': 3,
+        'title': 'One more thing',
+        'completed': false,
+        'editing': false,
+      },
+    ];
+  }
+
+}
