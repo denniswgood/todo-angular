7bf7071df1db9ed814631b59236b2a15115e12f0
Dennis Good <dgood@fusionalliance.com>
2019-04-18 21:32:33 -0400

    Adding delete and update field

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../interfaces/todo';
 
 @Component({
   selector: 'todo-list',
@@ -6,11 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
-  todos: object[];
+  todos: Todo[];
+  todoTitle: string;
+  idForTodo: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.idForTodo = 4;
+    this.todoTitle = '';
     this.todos = [
       {
         'id': 1,
@@ -31,6 +36,27 @@ export class TodoListComponent implements OnInit {
         'editing': false,
       },
     ];
+  }
+  addTodo(): void {
+    if (this.todoTitle.trim().length === 0) {
+      return;
+    }
+    this.todos.push({
+      id: this.idForTodo,
+      title: this.todoTitle,
+      completed: false,
+      editing: false
+    })
+    this.todoTitle = '';
+    this.idForTodo++;
+  }
+
+  editTodo(todo: Todo): void {
+    todo.editing = true;
+  }
+
+  deleteTodo(id: number): void {
+    this.todos = this.todos.filter(todo => todo.id !== id);
   }
 
 }
